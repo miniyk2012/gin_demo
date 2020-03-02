d4e3ee823ada3c9c5122cda3f49131e0f6af9981
杨恺 <yangkai02@qutoutiao.net>
2020-03-02 13:35:36 +0800

    run 2 server

--- go.mod
+++ go.mod
@@ -12,6 +12,7 @@ require (
 	github.com/mattn/go-isatty v0.0.12 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
+	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e
 	golang.org/x/sys v0.0.0-20200223170610-d5e6a3e2c0ae // indirect
 	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 	gopkg.in/go-playground/validator.v9 v9.31.0 // indirect
--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"time"
+	"golang.org/x/sync/errgroup"
 )
 
 func indexHandler(c *gin.Context) {
@@ -25,6 +26,7 @@ func m1(c *gin.Context) {
 	//	"message": "m1",
 	//})
 	fmt.Println(cost)
+	fmt.Println("out m1")
 }
 
 func m2(c *gin.Context) {
@@ -32,11 +34,27 @@ func m2(c *gin.Context) {
 	c.Set("name", "杨恺")
 }
 
-func main() {
+func m3Closure(use bool) gin.HandlerFunc {
+	return func (c *gin.Context) {
+		if use {
+			c.Next()
+		} else {
+			fmt.Println("in m3")
+			c.Next()
+			fmt.Println("out m3")
+		}
+	}
+}
+
+var (
+	g errgroup.Group
+)
+
+func router01() http.Handler {
 	e := gin.New()
 	e.Use(gin.Logger())
 	e.Use(gin.Recovery())
-	e.Use(m1)
+	e.Use(m1, m3Closure(true))
 	e.GET("/test", m2, func(c *gin.Context) {
 		name := c.MustGet("name").(string) // 从上下文取值
 		log.Println(name)
@@ -45,5 +63,42 @@ func main() {
 		})
 	})
 	e.GET("/index", indexHandler)
-	e.Run()
+	return e
+}
+
+func router02() http.Handler {
+	e := gin.New()
+	e.Use(gin.Recovery())
+	e.GET("/", func(c *gin.Context) {
+		c.JSON(
+			http.StatusOK,
+			gin.H{
+				"code":  http.StatusOK,
+				"error": "Welcome server 02",
+			},
+		)
+	})
+	return e
+}
+
+func main() {
+	server01 := &http.Server{
+		Addr: ":8080",
+		Handler:      router01(),
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+	server02 := &http.Server{
+		Addr: ":8081",
+		Handler:      router02(),
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+	g.Go(func() error {
+		return server01.ListenAndServe()
+	})
+	g.Go(func() error {
+		return server02.ListenAndServe()
+	})
+	g.Wait()
 }
